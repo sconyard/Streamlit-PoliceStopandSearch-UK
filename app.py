aad745435db39c67abfe9e3ed826837188262280
sconyard <simon@virtual-simon.co.uk>
2023-01-29 11:38:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.sidebar.info("Streamlit app that allows the exploration the UK Policing Stop 
 st.sidebar.markdown("The app works by running API requests to [Police Data API](https://data.police.uk/docs/).")
 st.sidebar.markdown("With filters for police force and month/year")
 st.sidebar.markdown("Information from the query is plot on to a map and is also presented in a table and a graph")
-st.sidebar.markdown("The graph tracks the object of search against the age range")
+st.sidebar.markdown("The graph tracks the object of search against the logged age range")
 st.sidebar.markdown("Information returned from the query can be downloaded as a CSV for later reference")
 
 def police_api():
